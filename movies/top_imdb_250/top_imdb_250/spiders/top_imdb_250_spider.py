07ed4565565986245e94382a111e659894ba0cbc
Mittu <mx.raspberry.pi@gmail.com>
2016-11-22 21:50:33 +0000

    scrape id's of top 250 imdb using scrapy

---
+++ movies/top_imdb_250/top_imdb_250/spiders/top_imdb_250_spider.py
@@ -0,0 +1,14 @@
+#
+# to run, from top-level dir: scrapy crawl top250 -o top250.json
+#
+
+import scrapy
+
+class TopImdb250(scrapy.Spider):
+    name = "top250"
+
+    start_urls = ['http://www.imdb.com/chart/top?sort=rk,asc&mode=simple']
+
+    def parse(self, response):
+        for each in response.css('td.titleColumn'):
+            yield { 'id' : each.css('a::attr(href)').re(r'/title/([\d\w]*)/?.*').pop() }
